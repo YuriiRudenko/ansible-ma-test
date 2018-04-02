713465ff4f981cf4a45cc0dfadda158a6de172fa
Yurii Rudenko <yurii.rudenko@masterofcode.com>
2018-04-02 20:38:11 +0300

    a

--- ansible/production.sh
+++ ansible/production.sh
@@ -1,7 +1,5 @@
-cd /home/app/ma/current && \
- docker-compose -f docker-compose.production.yml stop
- docker-compose -f docker-compose.production.yml rm -f -v && \
- docker run --rm current_rails rails db:create && \
- docker run --rm current_rails rails db:migrate && \
- docker-compose -f docker-compose.production.yml up -d && \
- docker ps
+cd /home/app/ma/current
+docker-compose -f docker-compose.production.yml stop
+docker-compose -f docker-compose.production.yml rm -f -v
+docker-compose -f docker-compose.production.yml up --build -d
+docker ps
