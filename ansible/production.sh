79b4e2fb6862032bd7e669ca6d8bc1b0a8ec62d6
yuriirudenko <yurii.rudenko@masterofcode.com>
2019-04-22 14:09:30 +0300

    fix

--- ansible/production.sh
+++
@@ -1,5 +0,0 @@
-cd /home/app/ma/current
-docker-compose -f docker-compose.production.yml stop
-docker-compose -f docker-compose.production.yml rm -f -v
-docker-compose -f docker-compose.production.yml up --build -d
-docker ps
