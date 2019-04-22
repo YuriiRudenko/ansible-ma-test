fdd700da28c31924faef6b23fc9f9ab3687a92ba
yuriirudenko <yurii.rudenko@masterofcode.com>
2019-04-22 14:26:19 +0300

    fix

--- ansible/production.sh
+++ ansible/production.sh
@@ -1,5 +1,8 @@
 cd /home/app/ma/current
+docker-compose -f docker-compose.production.yml down
+docker-compose -f docker-compose.production.yml build
+docker-compose -f docker-compose.production.yml run --rm rails rake db:create
+docker-compose -f docker-compose.production.yml run --rm rails rake db:migrate
 docker-compose -f docker-compose.production.yml stop
-docker-compose -f docker-compose.production.yml rm -f -v
 docker-compose -f docker-compose.production.yml up --build -d
 docker ps
